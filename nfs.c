8adfb47efb337d36e40116bd89df894ea4a55830
Pascal Schmidt <unfs3-server@ewetel.net>
2004-12-11 11:03:46 +0000

    Stupid variable declaration after code, again.
    git-svn-id: svn://svn.code.sf.net/p/unfs3/code/unfs3.svn/trunk@256 fb7659fb-8760-4330-86f7-fdbeb49e81a3

--- nfs.c
+++ nfs.c
@@ -45,9 +45,9 @@
  * otherwise zero result structure and return with error status
  */
 #define PREP(p,f) do {						\
+                      unfs3_fh_t *fh = (void *)f.data.data_val; \
                       setegid(0);                               \
                       seteuid(0);                               \
-                      unfs3_fh_t *fh = (void *)f.data.data_val; \
                       p = fh_decomp(f);				\
                       if (exports_options(p, rqstp, NULL, NULL) == -1) { \
                           memset(&result, 0, sizeof(result));	\
