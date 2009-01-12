fc8c9e5559591babde63f2f847270fcbbe2a7968
Pascal Schmidt <unfs3-server@ewetel.net>
2009-01-12 21:10:56 +0000

    Change definition of export_path to a declaration, so that this variable is not (multiply) defined by every source file that #includes the header
    git-svn-id: svn://svn.code.sf.net/p/unfs3/code/unfs3.svn/trunk@469 fb7659fb-8760-4330-86f7-fdbeb49e81a3

--- Config/exports.h
+++ Config/exports.h
@@ -22,7 +22,7 @@
 extern exports	exports_nfslist;
 /* Options cache */
 extern int	exports_opts;
-const char      *export_path; 
+extern const char *export_path; 
 extern uint32 	export_fsid;
 extern uint32   export_password_hash;
 
